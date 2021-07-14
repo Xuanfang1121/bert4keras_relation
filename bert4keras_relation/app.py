cc05c7ada3642d9b7c3220bb99dc9e019f0104fd
zhangxiaofang <zxfxuan@163.com>
2021-07-15 00:19:20 +0800

    fix:修改app.py中加载模型的问题

--- bert4keras_relation/app.py
+++ bert4keras_relation/app.py
@@ -4,10 +4,11 @@
 import json
 import requests
 
+import tensorflow as tf
 from flask import Flask
 from flask import jsonify
 from flask import request
-
+from tensorflow.python.keras.backend import set_session
 from utils import SPO
 from utils import extract_spoes
 from bert4keras.tokenizers import Tokenizer
@@ -16,6 +17,10 @@ from model import Bert4kerasRelationExtract
 
 app = Flask(__name__)
 app.config["JSON_AS_ASCII"] = False
+
+sess = tf.Session()
+set_session(sess)
+graph = tf.get_default_graph()
 
 maxlen = 128
 config_path = 'D:/spyder/pretrain_model/chinese_L-12_H-768_A-12/bert_config.json'
@@ -33,6 +38,9 @@ print("predicate2id :", predicate2id)
 print("relation number: ", len(predicate2id))
 
 tokenizer = Tokenizer(dict_path, do_lower_case=True)
+subject_model, object_model, train_model = Bert4kerasRelationExtract(config_path, checkpoint_path,
+                                                                         len(predicate2id)).get_model()
+train_model.load_weights('./models/baidu_relation_DuLE_1.0/best_model.weights')
 
 
 @app.route("/relation", methods=['POST'])
@@ -40,16 +48,18 @@ def model_predict():
     data = json.loads(request.get_data(), encoding="utf-8")
     text = data.get("text")
     # get model
-    # todo 每次请求加载一次模型，提前加载模型会报错，为什么
-    subject_model, object_model, train_model = Bert4kerasRelationExtract(config_path, checkpoint_path,
-                                                                         len(predicate2id)).get_model()
-    train_model.load_weights('./models/baidu_relation_DuLE_1.0/best_model.weights')
+    global sess
+    global graph
+    with graph.as_default():
+        set_session(sess)
+        pred = extract_spoes(text, maxlen, tokenizer, subject_model, object_model,
+                                               id2predicate)
 
-    R = set([SPO(spo) for spo in extract_spoes(text, maxlen, tokenizer, subject_model, object_model,
-                                               id2predicate)])
-    print(list(R))
+        # R = set([SPO(spo) for spo in extract_spoes(text, maxlen, tokenizer, subject_model, object_model,
+        #                                        id2predicate)])
+    print(pred)
     return jsonify({"text": text,
-                    "relation": list(R)})
+                    "relation": pred})
 
 
 if __name__ == "__main__":
